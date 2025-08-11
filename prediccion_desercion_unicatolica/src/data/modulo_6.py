d721801e789afea2d0fd0b1127a48f7aec0fc1d3
anfeMurillo <anfehumu2.0@hotmail.com>
2025-08-10 23:31:48 -0500

    Correcion de tipos en modulo_6.py

--- prediccion_desercion_unicatolica/src/data/modulo_6.py
+++ prediccion_desercion_unicatolica/src/data/modulo_6.py
@@ -5,8 +5,13 @@ path = p.Path('../../data/raw/datos.csv')
 
 df = pd.read_csv(path)
 
-df = df.apply(pd.to_numeric,errors='coerce').astype(float)
+cols_int = ['id','edad','genero','estrato','residencia','estado_civil','periodo_inicio','repitencias_total','desertor']
+for col in cols_int:
+	if col in df.columns:
+		df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0).astype(int)
+
+df['']
 
 destiny = p.Path('../../data/processed/data_set.csv')
 
-df.to_csv(destiny)
+df.to_csv(destiny, index=False)
