13baf951b064733871ff4c2e2300626a5dcd52e5
anfeMurillo <anfehumu2.0@hotmail.com>
2025-08-10 23:41:41 -0500

    modulo_6.py terminado

--- prediccion_desercion_unicatolica/src/data/modulo_6.py
+++ prediccion_desercion_unicatolica/src/data/modulo_6.py
@@ -5,12 +5,13 @@ path = p.Path('../../data/raw/datos.csv')
 
 df = pd.read_csv(path)
 
+df = df.apply(lambda x: pd.to_numeric(x, errors='coerce')).astype(float)
+
 cols_int = ['id','edad','genero','estrato','residencia','estado_civil','periodo_inicio','repitencias_total','desertor']
 for col in cols_int:
 	if col in df.columns:
-		df[col] = pd.to_numeric(df[col], errors='coerce').fillna(0).astype(int)
+		df[col] = df[col].fillna(0).astype(int)
 
-df['']
 
 destiny = p.Path('../../data/processed/data_set.csv')
 
